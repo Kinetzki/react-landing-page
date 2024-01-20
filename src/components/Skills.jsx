18b50e2c8ec3c4e769031d2b075a6f184ed1a0fd
Kenneth <kinetzki2k20@gmail.com>
2024-01-20 20:00:53 +0800

    spelling correction

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -214,7 +214,7 @@ function Skills(props) {
           }}
         />
       </BoxContainer>
-      <PageLocation page="Skills" current="Back-end Frameworks" />
+      <PageLocation page="Skills" current="Back-end Framework/s" />
       <IconHeader text="My Back-end Frameworks" intersect={backRef}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <path d="M64 32C28.7 32 0 60.7 0 96v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm280 72a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm48 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0zM64 288c-35.3 0-64 28.7-64 64v64c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V352c0-35.3-28.7-64-64-64H64zm280 72a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm56 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z" />
